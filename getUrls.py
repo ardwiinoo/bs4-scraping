939c1bbc70201558a99f4665fe259b26f9b386e4
arif <ardwino88@gmail.com>
2022-09-25 10:18:34 +0700

    add: membuat script scrap url

---
+++ getUrls.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# url,req,soup
+url = "http://uad.ac.id/"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+out = soup.find_all('a')
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+file = csv.writer(open("output.csv", "w"))
+file.writerow([soup.find_all('a')])
